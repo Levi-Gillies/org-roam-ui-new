c491be38dd470e9b9ffdfbe2e2572eb506f2cf9b
Levi Gillies <levigillies0@gmail.com>
2026-03-11 19:49:42 -0700

    bug fixes x6

--- util/getThemeColor.ts
+++ util/getThemeColor.ts
@@ -1,3 +1,6 @@
 export const getThemeColor = (name: string, theme: any) => {
-  return name.split('.').reduce((o, i) => o[i], theme.colors)
+  if (!name || name.startsWith('#') || name.startsWith('rgb')) {
+    return name
+  }
+  return name.split('.').reduce((o, i) => o?.[i], theme.colors) ?? name
 }
